96ce3a516b9c35256bf902c874459d60c5e70025
Joann Holland <joann.c.holland@btinternet.com>
2022-02-03 19:21:37 +0000

    initial commit

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import { Location } from "@material-ui/icons/LocationOnOutlined";
+import { LocationOnOutlined } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
+import mapStyles from "./mapStyles";
+
+const Map = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClicked,
+  weatherData,
+}) => {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery("(min-width:600px)");
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        zoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => {
+          setChildClicked(child);
+        }}
+      >
+        {places?.map((place, index) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={index}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlined color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt={place.name}
+                />
+                <Rating size="small" value={Number(place.rating)} />
+              </Paper>
+            )}
+          </div>
+        ))}
+        {weatherData?.list?.map((data, index) => {
+          <div key={index} lat={data.coord.lat} lng={data.coord.lon}>
+            <img
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              alt={data.weather[0].description}
+            />
+          </div>;
+        })}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
